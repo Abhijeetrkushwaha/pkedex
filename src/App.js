87a3da1dde6c7b7ed358c03951a692d0fb09f2ce
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2021-01-17 14:48:58 +0530

    added data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      hii
+    <div className="app">
+      <Nav />
     </div>
   );
 }
