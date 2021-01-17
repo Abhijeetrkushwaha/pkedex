f98599f2a35433a9587b46876bbc39c87692c214
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2021-01-17 20:00:32 +0530

    added data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CardImg from '../images/3.png'
+import CardImg from '../images/3.png';
 import { Link } from 'react-router-dom';
 
 function Card() {
--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,9 +1,20 @@
 import React from "react";
+import CardImg from '../images/3.png';
 
 function PokemonDetail() {
   return (
-    <div className="container">
-      hi
+    <div className="container text-center">
+      <div className="pokemon">
+        <div className="pokemon-id">
+          <h3>#1</h3>
+        </div>
+        <div className="pokemon-name">
+          <h2>Bulbarsaur</h2>
+        </div>
+        <div className="pokemon-img">
+          <img src={CardImg} alt=""/>
+        </div>
+      </div>
     </div>
   )
 }
