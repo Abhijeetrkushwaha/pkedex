76065676840db5a95c21eb6f12facbd526efc477
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2021-01-18 16:07:34 +0530

    added data

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -17,7 +17,7 @@ function PokemonDetail() {
             <h3>Type</h3>
             <div className="type-grp">
               <p>Poision</p>
-              <p>Grasss</p>
+              <p>Grass</p>
             </div>
           </div>
         </div>
@@ -27,55 +27,3 @@ function PokemonDetail() {
 }
 
 export default PokemonDetail;
-
-
-{/*  <div className="row pt-4">
-            <div className="col text-left pokemon-detail">
-              <h3>About</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corporis magni placeat ipsum harum molestias iste quibusdam laboriosam incidunt similique vero eos odit consequuntur facilis, error reprehenderit voluptate alias ipsa.</p>
-            </div>
-          </div>
-  <div className="row align-items-center">
-            <div className="col-12 col-lg-6">
-              
-            </div>
-            <div className="col-12 col-lg-6">
-              <div className="pokemon-id">
-                <h3>#1</h3>
-              </div>
-              <div className="pokemon-name">
-                <h2>Bulbarsaur</h2>
-              </div>
-              <div className="pokemon-type text-center">
-                <h3>Type</h3>
-                <div className="type-grp">
-                  <p>Poision</p>
-                  <p>Grass</p>
-                </div>
-              </div>
-              <div className="container">
-                <div className="row pokemon-about justify-content-center">
-                  <div className="col col-md-3 col-12">
-                    <div className="about-detail about-height">
-                      <h4>Height</h4>
-                      <p>111</p>
-                    </div>
-                  </div>
-                  <div className="col col-md-3 col-12">
-                    <div className="about-detail about-weight">
-                      <h4>Weight</h4>
-                      <p>25</p>
-                    </div>
-                  </div>
-                  <div className="col col-md-3 col-12">
-                    <div className="about-detail about-ability">
-                      <h4>Ability</h4>
-                      <p>fire</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> 
-
-        */}
