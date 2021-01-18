ff5e0a5791754be4d3dcc2ad5d767e8fc759bbf0
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2021-01-18 16:40:39 +0530

    added data

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -21,6 +21,32 @@ function PokemonDetail() {
                 <p>Grass</p>
               </div>
             </div>
+            <div className="pokemon-more-detail row justify-content-center">
+              <div className="col-4 col-lg-2 more-detail">
+                <h4>Height</h4>
+                <p>111</p>
+              </div>
+              <div className="col-4 col-lg-2 more-detail">
+                <h4>Weight</h4>
+                <p>23</p>
+              </div>
+              <div className="col-4 col-lg-2 more-detail">
+                <h4>Ability</h4>
+                <p>attack</p>
+              </div>
+              <div className="col-4 col-lg-2 more-detail">
+                <h4>Egg Group</h4>
+                <p>monster</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container about">
+          <div className="row">
+            <div className="col-12">
+              <h3>About</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo laudantium quibusdam, dolores optio harum, amet eaque incidunt error voluptatibus cumque dolorum. Enim quisquam ratione impedit quia error, veniam incidunt beatae.</p>
+            </div>
           </div>
         </div>
       </div>
